4d5e6ad4684d6a3f6f56c5aaf15c71e77c286b53
Nahar Singh <naharsingh151299@gmail.com>
2023-01-03 02:16:17 +0530

    basic version completed

--- src/components/Meals/AvilableMeals.js
+++ src/components/Meals/AvilableMeals.js
@@ -3,43 +3,49 @@ import classes from './AvailableMeals.module.css';
 import MealItem from './MealItem/MealItem';
 
 
-    const DUMMY_MEALS = [
-        {
-            id: 'm1',
-            name: 'Ras Gulle',
-            description: 'Finest fish and veggies',
-            price: 22.99,
-        },
-        {
-            id: 'm2',
-            name: 'Burger',
-            description: 'A german specialty!',
-            price: 16.5,
-        },
-        {
-            id: 'm3',
-            name: 'Pizza',
-            description: 'American, raw, meaty',
-            price: 12.99,
-        },
-        {
-            id: 'm4',
-            name: 'Cheese',
-            description: 'Healthy...and green...',
-            price: 18.99,
-        },
-    ];
+const DUMMY_MEALS = [
+    {
+        id: 'm1',
+        name: 'Ras Gulle',
+        description: 'Finest fish and veggies',
+        price: 15,
+    },
+    {
+        id: 'm2',
+        name: 'Burger',
+        description: 'A german specialty!',
+        price: 57,
+    },
+    {
+        id: 'm3',
+        name: 'Pizza',
+        description: 'American, raw, meaty',
+        price: 299,
+    },
+    {
+        id: 'm4',
+        name: 'Coffee',
+        description: 'Healthy...and green...',
+        price: 40,
+    },
+];
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(meal => <MealItem key={meal.id} name={meal.name} description={meal.name} price={meal.price} id={meal.id} />);
+    const mealsList = DUMMY_MEALS.map(meal => <MealItem
+        key={meal.id}
+        name={meal.name}
+        description={meal.name}
+        price={meal.price}
+        id={meal.id}
+    />);
 
-    return <section className={classes.meals}> 
-    <Card>
-        <ul> 
-            {mealsList} 
-            
+    return <section className={classes.meals}>
+        <Card>
+            <ul>
+                {mealsList}
 
-        </ul>
+
+            </ul>
         </Card>
     </section>
 
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,13 +1,35 @@
+import { useRef, useState } from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 const MealItemForm =  props=>{
 
+    const [ amoutIsValid , setAmountIsValid] = useState(true);
+        const amountInputRef = useRef();
+
+    const submitHandler = event => {
+        event.preventDefault();
+        const enteredAmount = amountInputRef.current.value;
+        const enteredAmountNumber  = +enteredAmount; // convert to number 
+        if(enteredAmount.trim().length === 0|| 
+        enteredAmountNumber < 1 || 
+        enteredAmountNumber > 5){
+            setAmountIsValid(false);
+            return;
+        }
+
+
+        props.onAddToCart(enteredAmountNumber);
+        
+
+    };
 
 
     return (
     <form className={classes.form}> 
 
-   <Input  label ={'Amount'} input ={{
+   <Input 
+                ref={amountInputRef}
+   label ={'Amount'} input ={{
         id: 'amount_'  + props.id,
         type: 'number',
         min:'1',
@@ -15,9 +37,10 @@ const MealItemForm =  props=>{
         step:'1',
         defaultValue : '1'
    }}></Input>
-    <button> 
+            <button onClick={submitHandler}> 
         + Add
     </button>
+    {!amoutIsValid && <p>Please Enter a valid amount (1-5). </p>}
 
 
 
